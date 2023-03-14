aaa5bc5386b9bab45c135102e781c924cc9b948c
Kiara Bodiu <bodyukiara@gmail.com>
2023-03-14 15:21:11 +0200

    Update telegram_bot_microhelper_var0.py

--- telegram_bot_microhelper_var0.py
+++ telegram_bot_microhelper_var0.py
@@ -21,16 +21,19 @@ events = ['Раптово відкрився портал у варп і з нь
           'Почався варп-шторм! Всі наступні спроби використати психічні сили можуть призвести до непередбачуваних наслідків.',
           'Біля вас відкрився портал у варп і з нього випала купа металобрухту. У ній ви розгледіли запечатаний ящик з боєприпасами.',
           'Відкрився портал у варп і з нього вилетіли демони! Непощастило так непощастило.',
-          'Біля вас відкрився портал в варп і з нього вийшов хаоситський титан класу "Гончак". КАПІТАЛЬНО непощастило.']
+          'Біля вас відкрився портал в варп і з нього вийшов хаоситський титан класу "Гончак". КАПІТАЛЬНО непощастило.',
+          'Ваші системи сканування космосу виявили новоутворену аномалію у безпосередній близькості.',
+          'Почалась електромагнітна буря. Потужність вокс-систем знизилась до мінімального рівня.',
+          'З порталу у варп просто вам на голову висипались ящики з емблемою імперської гвардії.']
 
-enemiesOrks0 = ['1 мекбойз', '1 варбосс', 'банка-убивця', '1 горканавт', '1 вартракк']
-enemiesOrks1 = ['2 бойзи з чоппами', '3 бойзи з вогнеметами', '3 гретчини', '2 вибухових сквіга', '5 снотлінгів']
+enemiesOrks0 = ['1 мекбойз (200хп/20ат)', '1 варбосс (400хп/30ат)', 'банка-убивця (380хп/30ат)', '1 горканавт (400хп/25ат)', '1 вартракк (420хп/30ат)']
+enemiesOrks1 = ['2 бойзи з чоппами (100хп/15ат)', '3 бойзи з вогнеметами (100хп/20ат)', '3 гретчини (50хп/5ат)', '2 вибухових сквіга (50хп/20ат)', '5 снотлінгів (15хп/2ат)']
 
-enemiesTyranids0 = ['1 тиран вулика', '1 зоантроп', '1 гарпія', '1 маліцептор']
-enemiesTyranids1 = ['2 ліктори', '3 гаунти', '2 пожирачі']
+enemiesTyranids0 = ['1 тиран вулика (400хп/40ат)', '1 зоантроп (180хп/30ат)', '1 гарпія (200хп/30ат)', '1 маліцептор (180хп/25ат)']
+enemiesTyranids1 = ['2 ліктори (110хп/20ат)', '3 гаунти (100хп/15ат)', '2 пожирачі (110хп/15ат)']
 
-enemiesChaos0 = ['1 Кровожер', '1 Великий Нечистий', 'Пекельний дракон', '3 психноєни', '1 Осквернювач']
-enemiesChaos1 = ['2 жахи Тзінча', '3 гончака Кхорна', '2 демонетки Слаанеш', '5 нурглінгів']
+enemiesChaos0 = ['1 Кровожер (200хп/30ат)', '1 Великий Нечистий (400хп/30ат)', 'Пекельний дракон (300хп/25ат)', '3 психноєни (110хп/20ат)', '1 Осквернювач (300хп/25ат)']
+enemiesChaos1 = ['2 жахи Тзінча (100хп/15ат)', '3 гончака Кхорна (100хп/20ат)', '2 демонетки Слаанеш (90хп/15ат)', '5 нурглінгів (40хп/5ат)']
 
 
 lootOrks = ['Купа непотрібного металобрухту',
@@ -47,17 +50,30 @@ lootChaos = ['1 дивна книга на замку. Якимось чином
              'Калюжа сірої жижи. Смердить огидно.',
              'Хмара синього туману. Близько підходити не рекомендується',
              '1 короткий меч зі склоподібної аморфної речовини. Використовувати на свій страх і ризик.']
-lootImperium = ['2 крак-гранати',
-                '2 фраг-гранати',
-                '1 лазган',
-                '1 ніж',
-                '2 повних магазини набоїв для болтера',
-                'Купа металобрухту',
-                '1 стаббер',
-                '1 мельта-бомба',
-                '1 плазмовий пістолет',
-                '3 повних магазини набоїв для стаббера',
-                '1 болт-пістолет',]
+lootImperium0 = ['2 крак-гранати',
+                 '2 фраг-гранати',
+                 '1 лазган',
+                 '1 ніж',
+                 '2 повних магазини набоїв для болтера',
+                 'купа металобрухту',
+                 '1 стаббер',
+                 '1 мельта-бомба',
+                 '1 плазмовий пістолет',
+                 '3 повних магазини набоїв для стаббера',
+                 '1 болт-пістолет']
+
+lootImperium1 = ['5 батончиків',
+                 '3 літрових пляшки води',
+                 '1 велика аптечка',
+                 '2 середніх аптечки',
+                 '2 маленьких аптечки',
+                 'купа непотрібного сміття',
+                 'промаслена ганчірка',
+                 'невелика ємність з прометієм',
+                 '1 вокс-кастер',
+                 'кількаметровий моток мотузки',
+                 'складний ніж',
+                 'уривок карти невідомого світу']
 
 def start(update, context):
     update.message.reply_text("Ваш мікропомічник у веденні ваховських кампаній.")
@@ -96,7 +112,7 @@ def rollCoin(update, context):
         context.bot.sendMessage(chat_id='-858506491', text=txmsg)
 
 def startEvents(update, context):
-    x = randint(0, 5)
+    x = randint(0, 8)
     if x == 0:
         context.bot.sendPhoto(chat_id='-858506491', caption=events[0], photo='https://warhammergames.ru/_pu/0/50128183.jpg')
         context.bot.sendMessage(chat_id='-858506491', text="Вороги:")
@@ -132,6 +148,18 @@ def startEvents(update, context):
         context.bot.sendMessage(chat_id='-858506491', text=tmsg)
     elif x == 5:
         context.bot.sendPhoto(chat_id='-858506491', caption=events[5], photo='https://64.media.tumblr.com/648f5486db45399e2d7a7ed06bdc1ca7/tumblr_pan8rlxXsq1uym32uo1_640.jpg')
+    elif x == 6:
+        context.bot.sendPhoto(chat_id='-858506491', caption=events[6], photo='https://todoestrategia.es/blog/wp-content/uploads/2022/10/eye_of_terror_by_darkrai696_d3dyd46-fullview.jpg')
+        context.bot.sendMessage(chat_id='-858506491', text="Вплив аномального випромінювання:")
+        x = randint(0, 1)
+        if x == 0:
+            context.bot.sendMessage(chat_id='-858506491', text="Потужність і площа покриття всіх систем сканування підвищена")
+        else:
+            context.bot.sendMessage(chat_id='-858506491', text="Потужність і площа покриття всіх систем сканування знижена")
+    elif x == 7:
+        context.bot.sendPhoto(chat_id='-858506491', caption=events[7], photo='https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/3a220e49-b640-4b89-95ad-221f76675213/dd2wgpp-e1886505-4c5e-4b51-801b-ab801eb7ca0f.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcLzNhMjIwZTQ5LWI2NDAtNGI4OS05NWFkLTIyMWY3NjY3NTIxM1wvZGQyd2dwcC1lMTg4NjUwNS00YzVlLTRiNTEtODAxYi1hYjgwMWViN2NhMGYucG5nIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.awA7WrQXDHkrsuytFNPzB9mPs0q2pu6wyhsv_kbZTk0')
+    elif x == 8:
+        context.bot.sendPhoto(chat_id='-858506491', caption=events[8], photo='https://images.dakkadakka.com/s/i/gallery/img/2016/5/14/799845.JPG')
     else:
         context.bot.sendMessage(chat_id='-858506491', text="Виникла помилка, усуваю скрапкод.")
 
@@ -147,8 +175,12 @@ def lootboxChaos(update, context):
     tmsg = random.choice(lootChaos)
     context.bot.sendMessage(chat_id='-858506491', text=tmsg)
 
-def lootboxImperium(update, context):
-    tmsg = random.choice(lootImperium) + ", " + random.choice(lootImperium) + ", " + random.choice(lootImperium)
+def lootboxImperium0(update, context):
+    tmsg = random.choice(lootImperium0) + ", " + random.choice(lootImperium0) + ", " + random.choice(lootImperium0)
+    context.bot.sendMessage(chat_id='-858506491', text=tmsg)
+
+def lootboxImperium1(update, context):
+    tmsg = random.choice(lootImperium1) + ", " + random.choice(lootImperium1)
     context.bot.sendMessage(chat_id='-858506491', text=tmsg)
 
 
@@ -167,7 +199,8 @@ dispatcher.add_handler(telegram.ext.CommandHandler('startEvents', startEvents))
 dispatcher.add_handler(telegram.ext.CommandHandler('lootboxOrks', lootboxOrks))
 dispatcher.add_handler(telegram.ext.CommandHandler('lootboxTyranids', lootboxTyranids))
 dispatcher.add_handler(telegram.ext.CommandHandler('lootboxChaos', lootboxChaos))
-dispatcher.add_handler(telegram.ext.CommandHandler('lootboxImperium', lootboxImperium))
+dispatcher.add_handler(telegram.ext.CommandHandler('lootboxImperium0', lootboxImperium0))
+dispatcher.add_handler(telegram.ext.CommandHandler('lootboxImperium1', lootboxImperium1))
 
 updater.start_polling()
 updater.idle()
